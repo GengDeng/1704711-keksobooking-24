ff30637acf6190038b373f9376920543f62cc26d
Denis <hallo-boy@mail.ru>
2021-09-29 00:43:34 +0300

    задание 2.15. финальные правки

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-'use strict'
-
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-
 getRandomInteger(0,15);
-// выводит случайное целое число от 0 до 15.
-
 
 const getRandomFloat = (min,max,numberAfterPoint) => {
   const numberAfterFormula = Math.random() * (max - min + 1) + min;
   return numberAfterFormula.toFixed(numberAfterPoint);
-}
-console.log(getRandomFloat(1,6,5));
-// выводит случайное число в диапазое от 1 до 6, с 5 знаками после запятой.
+};
+
+getRandomFloat(1,6,5);
 
